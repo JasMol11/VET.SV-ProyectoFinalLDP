40bd0010aa2add9cf670bb2eea18adfc60cd7bf3
yackiyacki <167651392+jacquelinerenderos@users.noreply.github.com>
2025-05-28 23:22:26 -0600

    Update treatment.h

---
+++ src/entities/treatment.h
@@ -0,0 +1,14 @@
+#ifndef TRATAMIENTO_H
+#define TRATAMIENTO_H
+
+#include <string>
+
+class Tratamiento {
+public:
+    std::string paciente;
+    std::string medicamento;
+    std::string dosis;
+    std::string periodo;
+};
+
+#endif // CLASE TRATAMIENTO_H
